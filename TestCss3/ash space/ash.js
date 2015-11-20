195daf3514c6b3562f263288b97efaafb1ae3cc5
Rendxx-Surface <Rendxx@gmail.com>
2015-11-20 16:38:15 -0600

    working on ash

---
+++ TestCss3/ash space/ash.js
@@ -0,0 +1,76 @@
+﻿/// <reference path="ash.html" />
+$(function () {
+    var scene = $('.scene');
+    var ashEle;
+    var rotate = {
+        x: 0,
+        y: 0,
+        z: 0
+    };
+
+    $('.btn-front').click(function () {
+        rotate.z += 10;
+        var transform = 'rotateX(' + rotate.x + 'deg) rotateY(' + rotate.y + 'deg) rotateZ(' + rotate.z + 'deg) scale(10)';
+        ashEle.css('transform', transform);
+    });
+    $('.btn-back').click(function () {
+        rotate.z -= 10;
+        var transform = 'rotateX(' + rotate.x + 'deg) rotateY(' + rotate.y + 'deg) rotateZ(' + rotate.z + 'deg) scale(10)';
+        ashEle.css('transform', transform);
+    });
+    $('.btn-right').click(function () {
+        rotate.y += 10;
+        var transform = 'rotateX(' + rotate.x + 'deg) rotateY(' + rotate.y + 'deg) rotateZ(' + rotate.z + 'deg) scale(10)';
+        ashEle.css('transform', transform);
+    });
+    $('.btn-left').click(function () {
+        rotate.y -= 10;
+        var transform = 'rotateX(' + rotate.x + 'deg) rotateY(' + rotate.y + 'deg) rotateZ(' + rotate.z + 'deg) scale(10)';
+        ashEle.css('transform', transform);
+    });
+    $('.btn-top').click(function () {
+        rotate.x += 10;
+        var transform = 'rotateX(' + rotate.x + 'deg) rotateY(' + rotate.y + 'deg) rotateZ(' + rotate.z + 'deg) scale(10)';
+        ashEle.css('transform', transform);
+    });
+    $('.btn-bottom').click(function () {
+        rotate.x -= 10;
+        var transform = 'rotateX(' + rotate.x + 'deg) rotateY(' + rotate.y + 'deg) rotateZ(' + rotate.z + 'deg) scale(10)';
+        ashEle.css('transform', transform);
+    });
+
+
+    var _createAsh = function (scale, x, y, z) {
+        var ash = $(_data.html.ash);
+        var transform = 'scale(10)';
+        ash.css({
+            'transform': transform
+        });
+        ash.appendTo(scene);
+        return ash;
+    };
+
+    var _data = {
+        html: {
+            ash: '<div class="ash"><div class="texture texture-1"></div><div class="texture texture-2"></div><div class="texture texture-3"></div><div class="texture texture-4"></div></div>'
+        },
+        range:{
+            scale:0.25,
+            x:400,
+            y:300,
+            z:1500
+        }
+    }
+
+
+
+    //////////////////////////////////
+
+    ashEle = _createAsh(10, 0, 0, 0);
+});
+
+
+var Particle = function () {
+    var _html = '<div class="particle"><div class="texture texture-1"></div><div class="texture texture-2"></div><div class="texture texture-3"></div><div class="texture texture-4"></div></div>';
+
+};
--- TestCss3/ash space/sample.js
+++ TestCss3/ash space/sample.js
@@ -1,31 +1,81 @@
 ﻿$(function () {
-    var cube = $('.cube');
+    // data
+    var scene = $('.scene');
+    var sceneRotate = {
+        x: 0,
+        y: 0,
+        z: 0
+    };
+    var ashTransform = [];
+    var ashEle = [];
+    var count = 100;
+
+    // button control
     $('.btn-front').click(function () {
-        cube.attr('class', 'cube front_');
+        sceneRotate.z -= 10;
+        _render();
     });
     $('.btn-back').click(function () {
-        cube.attr('class', 'cube back_');
+        sceneRotate.z += 10;
+        _render();
     });
     $('.btn-right').click(function () {
-        cube.attr('class', 'cube right_');
+        sceneRotate.y += 10;
+        _render();
     });
     $('.btn-left').click(function () {
-        cube.attr('class', 'cube left_');
+        sceneRotate.y -= 10;
+        _render();
     });
     $('.btn-top').click(function () {
-        cube.attr('class', 'cube top_');
+        sceneRotate.x += 10;
+        _render();
     });
     $('.btn-bottom').click(function () {
-        cube.attr('class', 'cube bottom_');
+        sceneRotate.x -= 10;
+        _render();
     });
 
+    var getRandom = function(range){
+        return Math.random() * range * 2 - range;
+    };
 
+    var _render = function () {
+        for (var i = 0; i < count; i++) {
+            var transform = 'rotateX(' + sceneRotate.x + 'deg) rotateY(' + sceneRotate.y + 'deg) rotateZ(' + sceneRotate.z + 'deg) translateX(' + ashTransform[i].x + 'px) translateY(' + ashTransform[i].y + 'px) translateZ(' + ashTransform[i].z + 'px) scale(' + ashTransform[i].scale + ')';
+            ashEle[i].css({
+                'transform': transform
+            });
+        }
+    };
 
+    var build = function (scene, count) {
+        for (var i = 0; i < count; i++) {
+            ashTransform[i] ={
+                scale: getRandom(_data.range.scale) + 0.5,
+                x: Math.floor(getRandom(_data.range.x)),
+                y: Math.floor(getRandom(_data.range.y)),
+                z: Math.floor(getRandom(_data.range.z))
+            };
+            ashEle[i] = $(_data.html.ash).appendTo(scene);
+        }
+        _render();
+    };
 
-
+    // parameter
     var _data = {
         html: {
-            '<div class="ash"><div class="texture texture-x"></div><div class="texture texture-y"></div><div class="texture texture-z"></div></div>'
+            ash: '<div class="ash"><div class="texture texture-x"></div><div class="texture texture-y"></div><div class="texture texture-z"></div></div>'
+        },
+        range: {
+            scale: 0.25,
+            x: 400,
+            y: 300,
+            z: 1500
         }
     }
+
+    //////////////////////////////////
+
+    build(scene, count);
 });

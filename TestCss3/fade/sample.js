e4513da34992f86a366b98e365b3bdaa46b0891a
Rendxx <rendxx@gmail.com>
2015-11-19 18:07:16 -0600

    Css3 Fade test

---
+++ TestCss3/fade/sample.js
@@ -0,0 +1,10 @@
+﻿$(function () {
+    var test = $('.test-wrap');
+    test.bind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function () { console.log(test.hasClass('hide')); });
+    $('.btn-1').click(function () {
+        test.removeClass('hide');
+    });
+    $('.btn-2').click(function () {
+        test.addClass('hide');
+    });
+});

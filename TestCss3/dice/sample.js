46e488865c0dba55bdc186e99872c6528b380e7f
Rendxx <rendxx@gmail.com>
2015-11-20 12:45:50 -0600

    cube

---
+++ TestCss3/dice/sample.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+    var cube = $('.cube');
+    $('.btn-front').click(function () {
+        cube.attr('class', 'cube front_');
+    });
+    $('.btn-back').click(function () {
+        cube.attr('class', 'cube back_');
+    });
+    $('.btn-right').click(function () {
+        cube.attr('class', 'cube right_');
+    });
+    $('.btn-left').click(function () {
+        cube.attr('class', 'cube left_');
+    });
+    $('.btn-top').click(function () {
+        cube.attr('class', 'cube top_');
+    });
+    $('.btn-bottom').click(function () {
+        cube.attr('class', 'cube bottom_');
+    });
+});

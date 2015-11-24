8e9400235b100b2a7fbc2686e843fcbc429b7c63
Rendxx <rendxx@gmail.com>
2015-11-24 12:20:00 -0600

    mouse control v1

--- TestCss3/butterfly/sample.js
+++ TestCss3/butterfly/sample.js
@@ -1,6 +1,7 @@
 ﻿$(function () {
     // data
-    var scene = $('.containor')
+    var containor = $('.containor')
+    var scene = $('.scene')
     var butterfly = $('.butterfly');
     var sceneRotate = {
         x: 0,
@@ -40,12 +41,54 @@
     });
 
     // mouse control
+    // get mouse position fomr mouse event
+    var _getMousePosition = function (e) {
+        var pos = {};
+        pos["x"] = e.pageX == undefined ? e.originalEvent.touches[0].pageX : e.pageX;
+        pos["y"] = e.pageY == undefined ? e.originalEvent.touches[0].pageY : e.pageY;
+        return pos;
+    };
 
+    var _lastPos = null;
+    var startDrag = function (e) {
+        e.preventDefault();
+        _lastPos = _getMousePosition(e);
+
+        containor.bind("mousemove", onDrag);
+        containor.bind("mouseup", stopDrag);
+    };
+
+    var onDrag = function (e) {
+        e.preventDefault();
+        var mousePos = _getMousePosition(e);
+        sceneRotate.x -= mousePos.y - _lastPos.y;
+        sceneRotate.y += mousePos.x - _lastPos.x;
+        _lastPos = mousePos;
+        _render();
+    };
+    var stopDrag = function (e) {
+        containor.unbind("mousemove", onDrag);
+        containor.unbind("mouseup", stopDrag);
+
+        var mousePos = _getMousePosition(e);
+        var mousePos = _getMousePosition(e);
+        sceneRotate.x -= mousePos.y - _lastPos.y;
+        sceneRotate.y += mousePos.x - _lastPos.x;
+        _lastPos = mousePos;
+        _render();
+    };
+
+    containor.bind("mousedown", startDrag);
 
 
     // function
+    var _renderScene = function () {
+        scene.css({
+            'transform': 'rotateX(' + sceneRotate.x + 'deg) rotateY(' + sceneRotate.y + 'deg) rotateZ(' + sceneRotate.z + 'deg) translate3D(' + sceneTranslate.x + 'px, ' + sceneTranslate.y + 'px, ' + sceneTranslate.z + 'px)'
+        });
+    };
     var _render = function () {
-        butterfly.css({
+        scene.css({
             'transform': 'rotateX(' + sceneRotate.x + 'deg) rotateY(' + sceneRotate.y + 'deg) rotateZ(' + sceneRotate.z + 'deg) translate3D(' + sceneTranslate.x + 'px, ' + sceneTranslate.y + 'px, ' + sceneTranslate.z + 'px)'
         });
     };

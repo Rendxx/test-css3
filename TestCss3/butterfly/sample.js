dd85adc894efc8c3153094ba043eedfe9b69f3c4
Rendxx <rendxx@gmail.com>
2015-11-24 11:40:01 -0600

    Build butterfly basic modle

---
+++ TestCss3/butterfly/sample.js
@@ -0,0 +1,47 @@
+﻿$(function () {
+    // data
+    var butterfly = $('.butterfly');
+    var sceneRotate = {
+        x: 0,
+        y: 0,
+        z: 0
+    };
+    var sceneTranslate = {
+        x: 0,
+        y: 0,
+        z: 0
+    };
+
+    // controller
+    $('.btn-front').click(function () {
+        sceneRotate.z -= 10;
+        _render();
+    });
+    $('.btn-back').click(function () {
+        sceneRotate.z += 10;
+        _render();
+    });
+    $('.btn-right').click(function () {
+        sceneRotate.y += 10;
+        _render();
+    });
+    $('.btn-left').click(function () {
+        sceneRotate.y -= 10;
+        _render();
+    });
+    $('.btn-top').click(function () {
+        sceneRotate.x += 10;
+        _render();
+    });
+    $('.btn-bottom').click(function () {
+        sceneRotate.x -= 10;
+        _render();
+    });
+
+
+    var _render = function () {
+        butterfly.css({
+            'transform': 'rotateX(' + sceneRotate.x + 'deg) rotateY(' + sceneRotate.y + 'deg) rotateZ(' + sceneRotate.z + 'deg) translate3D(' + sceneTranslate.x + 'px, ' + sceneTranslate.y + 'px, ' + sceneTranslate.z + 'px)'
+        });
+    };
+});

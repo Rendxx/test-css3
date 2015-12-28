50ee354e8a8b251298aa4d5826ca7198069b48e6
Rendxx <rendxx@gmail.com>
2015-12-28 15:05:40 -0600

    Test css3 calback

---
+++ TestCss3/Callback/sample.js
@@ -0,0 +1,46 @@
+﻿$(function () {
+    var transitionEnd = (function () {
+        var el = $('.test-field')[0];
+        var transEndEventNames = {
+            'WebkitTransition' : 'webkitTransitionEnd',
+            'MozTransition'    : 'transitionend',
+            'OTransition'      : 'oTransitionEnd otransitionend',
+            'transition'       : 'transitionend'
+        };
+
+        for (var name in transEndEventNames) {
+            if (el.style[name] !== undefined) {
+                return transEndEventNames[name];
+            }
+        }
+
+        return null;
+    })();
+
+    var end_show = function () {
+        msg.html("show");
+    };
+    var end_hide = function () {
+        msg.html("hide");
+    };
+
+    var test = $('.test-field');
+    var msg = $('.test-msg');
+    //test.bind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function () { console.log(test.hasClass('hide')); });
+    $('.btn-1').click(function () {
+        test.removeClass('hide');
+        if (transitionEnd != null) {
+            test[0].removeEventListener(transitionEnd, end_hide, false);
+            test[0].addEventListener(transitionEnd, end_show, false);
+        }
+    });
+    $('.btn-2').click(function () {
+        test.addClass('hide');
+        if (transitionEnd != null) {
+            test[0].removeEventListener(transitionEnd, end_show, false);
+            test[0].addEventListener(transitionEnd, end_hide, false);
+        }
+    });
+
+
+});
